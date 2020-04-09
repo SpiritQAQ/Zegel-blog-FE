3ac582b00e053c38bc3217589b0c53e3de25c188
Zegel <spiritqaq@gmail.com>
2020-04-09 18:07:21 +0800

    fixed: header gradient bgc ok

--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -7,28 +7,36 @@ import * as React from 'react'
 const _ = require('lodash/function');
 
 const stickyFunc = () => {
+
   // 监听滚动控制header背景颜色回调函数
-  const className = 'header-transparent'
+  const className = 'header-bg-transparent'
+  const gradientClassName = 'header-bg-gradient'
   if (window.pageYOffset > 500) return
   if (window.pageYOffset > 100) {
-    (document.querySelector('header') as HTMLInputElement).classList.remove(className)
+    (document.querySelector('.header-bg') as HTMLInputElement).classList.remove(className);
+    (document.querySelector('.header-bg') as HTMLInputElement).classList.add(gradientClassName)
   } else {
-    (document.querySelector('header') as HTMLInputElement).classList.add(className)
+    (document.querySelector('.header-bg') as HTMLInputElement).classList.add(className);
+    (document.querySelector('.header-bg') as HTMLInputElement).classList.remove(gradientClassName)
   }
 }
 
-
 export default class Header extends React.Component {
+
+  showLoginContainer() {
+
+  }
   componentDidMount() {
     window.addEventListener('scroll', _.throttle(stickyFunc, 100))
   }
+
   componentWillUnmount() {
     console.log('componentWillUnmount')
     window.removeEventListener('scroll', _.throttle(stickyFunc, 100))
   }
   render() {
     return (
-      <header className="header-transparent">
+      <header>
         <ul className="header-content">
           <li>
             <Link to="/">首页</Link>
@@ -39,7 +47,11 @@ export default class Header extends React.Component {
           <li>
             <Link to="/about">关于</Link>
           </li>
+          <li>
+            <div onClick={this.showLoginContainer}>登录</div>
+          </li>
         </ul>
+        <div className="header-bg header-bg-transparent "></div>
       </header>
     )
   }
